b3e3a7bbe776838f0d713b55f717127618bf29d6
Gaius Mathew <gaiusmathew@suuchi.com>
2023-08-25 22:49:57 +0530

    participant added

---
+++ components/accounts/data.ts
@@ -0,0 +1,28 @@
+export const columns = [
+   {name: 'Date', uid: 'date'},
+   {name: 'Breakfast', uid: 'breakfast'},
+   {name: 'Lunch', uid: 'lunch'},
+   {name: 'Dinner', uid: 'dinner'},
+];
+export const users = [
+   {
+      id: 1,
+      name: 'Tony Reichert',
+      role: 'CEO',
+      team: 'Management',
+      status: 'active',
+      age: '29',
+      avatar: 'https://i.pravatar.cc/150?u=a042581f4e29026024d',
+      email: 'tony.reichert@example.com',
+   },
+   {
+      id: 2,
+      name: 'Zoey Lang',
+      role: 'Technical Lead',
+      team: 'Development',
+      status: 'paused',
+      age: '25',
+      avatar: 'https://i.pravatar.cc/150?u=a042581f4e29026704d',
+      email: 'zoey.lang@example.com',
+   },
+];
